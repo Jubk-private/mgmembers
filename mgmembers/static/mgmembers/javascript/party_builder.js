5d2d10c4abfd5742502831f46452bc98c3809f5a
Jørgen Ulrik B. Krag <ju.krag@gmail.com>
2019-01-20 11:58:25 +0100

    Make all mentions of a character disappear when selected for a party

--- mgmembers/static/mgmembers/javascript/party_builder.js
+++ mgmembers/static/mgmembers/javascript/party_builder.js
@@ -9,28 +9,28 @@
     $('#partybuilder span.event-job').on("click", function() {
         var $this = $(this),
             $dest = destinations[$this.attr("data-role")],
+            charname = $this.attr("data-char-name"),
+            pickers = $(
+                '#partybuilder span.event-job[data-char-name=' + charname + ']'
+            ),
             $new_elem;
 
         if(!$dest) {
             return;
         }
 
-        $this.hide();
+        pickers.hide();
         $new_elem = $('<span class="selected-char"/>')
             .attr("data-job", $this.attr("data-job"))
-            .attr("data-char-name", $this.attr("data-char-name"))
+            .attr("data-char-name", charname)
             .attr("data-role", $this.attr("data-role"))
             .text(
                   $this.attr("data-char-name") +
                   " (" + $this.attr("data-job") + ")"
             );
         $new_elem.on("click", function() {
-            var $this = $(this),
-                name = $this.attr("data-char-name");
-            $(
-                '#partybuilder span.event-job[data-char-name=' + name + ']'
-            ).show();
-            $this.remove();
+            $(this).remove();
+            pickers.show();
         });
         $dest.append($new_elem);
     });
